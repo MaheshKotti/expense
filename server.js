8c36304ba8748ec4685db18441d1ac802cb25f8f
UmamaheshKotti <umamaheshkotti83@gmail.com>
2024-07-14 19:03:15 +0530

    Updated server.js file for Cors issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-// const cors = require('cors');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRouter = require('./routes/userRouter');
 const expenseRouter = require("./routes/expenseRouter");
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoUrl = process.env.MONGO_URL;
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/user", userRouter);
